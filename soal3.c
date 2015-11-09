ab5c473be1b45fcc80376c5027ef7bd5d73a6c42
syukronrm <syukronrifai@gmail.com>
2015-11-09 13:59:09 +0700

    logic fail solved :)

--- soal3.c
+++ soal3.c
@@ -7,57 +7,51 @@ static int status;
 
 void *thread_01()
 {
-    printf("thread 1 started\n");
+    //printf("thread 1 started\n");
     FILE *f1, *f2;
+    char line[101];
     f1 = fopen("file1.txt", "r");
     f2 = fopen("file2.txt", "a");
-    while (1)
+    while (fgets(line, 100, f1))
     {
-        char line[101];
-        if (fgets(line, 100, f1) != NULL)
-        {
-            fprintf(f2, "%s", line);
-	    printf("file1 > file2 : %s", line);
-        }
-        else
-            break;
+        fprintf(f2, "%s", line);
+	//printf("file1 > file2 : %s", line);
     }
     fclose(f1);
     fclose(f2);
-    printf("thread 1 stopped\n");
+    //printf("thread 1 stopped\n");
 }
 
 void *thread_02()
 {
-    printf("thread 2 started\n");
+    //printf("thread 2 started\n");
     char line[101];
     FILE *f2, *f3;
-    f2 = fopen("file2.txt", "r+");   // masalah!!!
+    f2 = fopen("file2.txt", "r+");
     f3 = fopen("file3.txt", "a");
-    printf("while thread 2 started\n");
-    printf("thread 2 status = %d\n", status);
-    while (!feof(f2) && status != 1)
+    //printf("while thread 2 started\n");
+    //printf("thread 2 status = %d\n", status);
+    while (!(!fgets(line, 100, f2) && status == 1))
     {
-        fgets(line, 100, f2);
-        printf("Loop status = %d\n", status);
+        //printf("Loop status = %d\n", status);
         fprintf(f3, "%s", line);
-	printf("file2 > file3 : %s", line);
+	//printf("file2 > file3 : %s", line);
     }
-    printf("thread 2 stopped\n");
+    //printf("thread 2 stopped\n");
     fclose(f2);
     fclose(f3);
 }
 
 int main()
 {
-    pthread_t threadid1, threadid2;
+    pthread_t threadid[2];
 
     status = -1;
-    pthread_create(&threadid1, NULL, thread_01, NULL);
-    pthread_create(&threadid2, NULL, thread_02, NULL);
-    pthread_join(threadid1, NULL);
+    pthread_create(&threadid[0], NULL, thread_01, NULL);
+    pthread_create(&threadid[1], NULL, thread_02, NULL);
+    pthread_join(threadid[0], NULL);
     status = 1;
-    printf("main status = %d\n", status);
-    pthread_join(threadid2, NULL);
+    //printf("main status = %d\n", status);
+    pthread_join(threadid[1], NULL);
     return 0;
 }
